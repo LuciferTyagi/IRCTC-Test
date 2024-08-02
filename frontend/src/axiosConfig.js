26ebdbcfaa95c76c361f673c187d7f0e67e01aa7
Yagyansh Tyagi <78792172+LuciferTyagi@users.noreply.github.com>
2024-08-02 14:29:23 +0530

    correct

--- frontend/src/axiosConfig.js
+++ frontend/src/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'https://irctc-test-server-fpeug9343-lucifertyagis-projects.vercel.app/api', 
+  baseURL: 'https://irctc-test-server.vercel.app/api', 
 });
 
 export default instance;
